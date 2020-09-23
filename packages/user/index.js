9e5cb807d2f4c90c7ed227f31c0e7654aa06f319
Guilherme-Aguiar <gui9627oli@gmail.com>
2020-09-23 00:01:12 -0300

    changes docker-compose

--- packages/user/index.js
+++ packages/user/index.js
@@ -1,4 +1,14 @@
 const express = require('express') 
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('postgres://developer:developer@172.23.0.2:5432/dev_database')
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
 const app = express();
 
