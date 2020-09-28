8a6ae8a7a572c31eda943a5e769004a39321ba27
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-09-28 15:16:11 -0300

    Refactor database configuration

--- packages/resolution/src/server.js
+++ packages/resolution/src/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
-
-//const Post = require('./models/Post');
+const db = require('./config/database');
 
 const app = express();
+
 const { Sequelize } = require('sequelize');
 
 app.use(express.json());
 app.use(routes);
 
-const sequelize = new Sequelize('postgres://developer:developer@172.23.0.2:5432/dev_database')
+const sequelize = new Sequelize(db)
 
 try {
     sequelize.authenticate();
@@ -17,8 +17,6 @@ try {
   } catch (error) {
     console.error('Unable to connect to the database:', error);
 }
-
-// Post.init(sequelize);
 
 app.listen(3002, () => console.log('servidor rodando na porta 3002'));
 
