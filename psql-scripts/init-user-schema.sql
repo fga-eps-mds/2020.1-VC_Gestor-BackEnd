b2fb2ecbc656e139aef695a24dcc0d06eb7c3ff7
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-10-16 20:55:27 -0300

    Change password string lenght

--- psql-scripts/init-user-schema.sql
+++ psql-scripts/init-user-schema.sql
@@ -5,14 +5,14 @@ CREATE TABLE users.user (
 	"name" varchar(25) NOT NULL,
 	"surname" varchar(25) NOT NULL,
 	"username" varchar(25) NOT NULL,
-	"password" varchar(25) NOT NULL,
+	"password" varchar(60) NOT NULL,
 	CONSTRAINT "User_pk" PRIMARY KEY ("user_id")
 );
 
 INSERT INTO users."user"
 (user_id, "name", surname, username, "password")
-VALUES(1, 'teste', 'teste', 'teste', '123123');
+VALUES(1, 'admin', 'admin', 'admin', '$2a$08$RjS04WuZEmMoP2OzSrz5x.zHVigMMOZR7feKi8jDhxQRfY2oKH4Gu');
 
 INSERT INTO users."user"
 (user_id, "name", surname, username, "password")
-VALUES(2, 'teste2', 'teste2', 'teste2', '123123');
+VALUES(2, 'admin2', 'admin2', 'admin2', '$2a$08$RjS04WuZEmMoP2OzSrz5x.zHVigMMOZR7feKi8jDhxQRfY2oKH4Gu');
