e351ca42d897bf3d0e116cc80a1f11ee2f8d2759
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-10-16 20:19:06 -0300

    Will populate users

--- psql-scripts/init-user-schema.sql
+++ psql-scripts/init-user-schema.sql
@@ -2,9 +2,17 @@ CREATE SCHEMA users;
 
 CREATE TABLE users.user (
 	"user_id" integer NOT NULL,
-	"name" varchar(20) NOT NULL,
-	"surname" varchar(20) NOT NULL,
-	"username" varchar(20) NOT NULL,
-	"password" varchar(20) NOT NULL,
+	"name" varchar(25) NOT NULL,
+	"surname" varchar(25) NOT NULL,
+	"username" varchar(25) NOT NULL,
+	"password" varchar(25) NOT NULL,
 	CONSTRAINT "User_pk" PRIMARY KEY ("user_id")
 );
+
+INSERT INTO users."user"
+(user_id, "name", surname, username, "password")
+VALUES(1, 'teste', 'teste', 'teste', '123123');
+
+INSERT INTO users."user"
+(user_id, "name", surname, username, "password")
+VALUES(2, 'teste2', 'teste2', 'teste2', '123123');
