0b2dd3bd34666e4c1ab4478ec55bdca2c81f51ce
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-10-20 01:25:31 -0300

    Add docker news container and database news schema

---
+++ psql-scripts/init-news-schema.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA news;
+
+CREATE TABLE news.news (
+	"news_id" SERIAL NOT NULL,
+	"title" varchar(25) NOT NULL,
+	"subtitle" varchar(25) NOT NULL,
+	"text" varchar(500) NOT NULL,
+	"image1" varchar(50) NOT NULL,
+	"image2" varchar(50) NOT NULL,
+	"image3" varchar(50) NOT NULL,
+	CONSTRAINT "User_pk" PRIMARY KEY ("news_id")
+);
