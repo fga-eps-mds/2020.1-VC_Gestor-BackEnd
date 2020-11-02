0c2d363a5d801e3181301c9d027c1d712e7bd817
Bruno Nunes <brunocmo.nunes@gmail.com>
2020-11-02 17:21:21 -0300

    Add delete newsById

--- packages/news/src/controller/NewsController.js
+++ packages/news/src/controller/NewsController.js
@@ -76,5 +76,17 @@ module.exports = {
     });
 
     return response.json(news);
+  },
+
+  async deleteNewsById(request, response) {
+    const { news_id } = request.params;
+
+    const news = await News.destroy({
+      where: {
+        news_id
+      }
+    });
+
+    return response.json(news);
   }
 };
