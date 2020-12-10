26241676f5b7ec579ae70bedec7e73ec2d2dd772
Guilherme-Aguiar <gui9627oli@gmail.com>
2020-12-09 22:46:47 -0300

    deletes post package

--- packages/resolution/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const routes = express.Router();
-
-const postsRouter = require("./posts.routes");
-
-routes.use("/posts", postsRouter);
-
-module.exports = routes;
