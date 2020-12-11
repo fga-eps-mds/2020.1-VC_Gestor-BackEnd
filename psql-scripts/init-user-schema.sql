e466675d18d31e6e058ba97ccc767db61ebe2653
Guilherme-Aguiar <gui9627oli@gmail.com>
2020-12-11 00:10:16 -0300

    changes http for testing

--- psql-scripts/init-user-schema.sql
+++ psql-scripts/init-user-schema.sql
@@ -23,3 +23,12 @@ VALUES(2, 'admin2', 'admin2', 'admin2', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5
 INSERT INTO users."user"
 (userid, "name", surname, username, "password", coderetrieve, email)
 VALUES(3, 'Vitor', 'Sulzbach', 'Sulzbach', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'aaaaaaaa', 'vjsulzbach@gmail.com');
+
+INSERT INTO users."user"
+(userid, "name", surname, username, "password", coderetrieve, email)
+VALUES(4, 'Guilherme', 'Aguiar', 'gui', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'aaaaaaaa', 'gui9627oli@gmail.com');
+
+INSERT INTO users."user"
+(userid, "name", surname, username, "password", coderetrieve, email)
+VALUES(5, 'Mateus', 'Sousa', 'mateus', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'aaaaaaaa', 'mateusaugusto-2009@gmail.com');
+
